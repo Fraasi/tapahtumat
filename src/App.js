e04a2499d753e3ebfcb4d3ab4eea54ba7585168c
Fraasi <doofassi@gmail.com>
2020-03-19 16:39:15 +0200

    localstorage for darkmode

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ function App() {
   const [showOnlyPispalaVenues, setShowOnlyPispalaVenues] = useState(true)
 
   useEffect(() => {
+    const localDarkMode = localStorage.getItem('dark_mode')
+    const preferDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    if (localDarkMode === 'false' || (localDarkMode === null && preferDarkMode === false)) {
+      setDarkMode(false)
+      localStorage.setItem('dark_mode', 'false')
+    } else if (localDarkMode === 'true' || preferDarkMode) {
+      setDarkMode(true)
+      localStorage.setItem('dark_mode', 'true')
+    }
+
     client.auth.loginWithCredential(new AnonymousCredential())
       .then(user => {
         return db.collection('pispala').find({}, { data: 1, _id: 0 }).asArray()
@@ -28,14 +39,18 @@ function App() {
         const hietis = events_data.splice(hietisIndex, 1)[0]
         const sortedEvents = events_data.sort((first, second) => first.name < second.name ? -1 : 1)
         sortedEvents.push(hietis)
-        console.log('data', sortedEvents)
+        if (process.env.NODE_ENV !== "production") console.log('Events:', sortedEvents)
         setEvents(() => sortedEvents)
         // only way to pass data to LMap, 'cos of the way
         // leaflet works, no props no context :(
         window.map_data = map_data
       }).catch(err => {
         console.error('Data fetch error:', err)
-        setErrorMsg(() => err)
+        if (err.toString().includes('TRANSPORT_ERROR')) {
+          setErrorMsg(() => 'Hupsista. Ei saatu yhteyttä tietokantaan. Tarkista nettiyhteytesi ja kokeile ladata sivu uudestaan.')
+        } else {
+          setErrorMsg(() => err.toString())
+        }
       })
   }, [])
 
@@ -43,22 +58,27 @@ function App() {
     setSwitch(prev => !prev)
   }
 
-  const toggleTheme = () => { setDarkMode(prev => !prev) }
+  const toggleTheme = () => {
+    setDarkMode(prev =>  {
+    localStorage.setItem('dark_mode', !prev)
+      return !prev
+    })
+  }
 
   return (
     <div className={`App ${isDarkMode ? 'theme-dark' : 'theme-light'}`}>
       <Header
-          isSwitchOn={isSwitchOn}
-          onSwitchChange={onSwitchChange}
-          setShowOnlyPispalaVenues={setShowOnlyPispalaVenues}
-          toggleTheme={toggleTheme}
-          isDarkMode={isDarkMode}
+        isSwitchOn={isSwitchOn}
+        onSwitchChange={onSwitchChange}
+        setShowOnlyPispalaVenues={setShowOnlyPispalaVenues}
+        toggleTheme={toggleTheme}
+        isDarkMode={isDarkMode}
       >
         Pispalan Tapahtumat
       </ Header>
       {
         errorMsg !== null
-          ? (<div className="error-loader">{errorMsg.toString()}</div>)
+          ? (<div className="error-loader">{errorMsg}</div>)
           : events === null
             ? (<div className="error-loader">
               Haetaan tapahtumia...<br /><br />
@@ -67,15 +87,15 @@ function App() {
             : (<div>
               {
                 events.map((el) => {
-                    return (
-                      <Event
-                        data={el}
-                        key={el.name}
-                        showOnlyPispalaVenues={showOnlyPispalaVenues}
-                        isSwitchOn={isSwitchOn}>
-                      </Event>
-                    )
-                  })
+                  return (
+                    <Event
+                      data={el}
+                      key={el.name}
+                      showOnlyPispalaVenues={showOnlyPispalaVenues}
+                      isSwitchOn={isSwitchOn}>
+                    </Event>
+                  )
+                })
               }
             </div>)
       }
