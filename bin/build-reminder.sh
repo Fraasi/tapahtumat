830cf163131c4d936819bdd4fe8248e06294e031
Fraasi <doofassi@gmail.com>
2020-04-19 22:54:36 +0300

    better msg

--- bin/build-reminder.sh
+++ bin/build-reminder.sh
@@ -1,16 +1,19 @@
-###!/usr/bin/bash
+###!/bin/sh
 
 # An example hook script to verify what is about to be committed.
 # Called by "git commit" with no arguments.  The hook should
 # exit with non-zero status after issuing an appropriate message if
 # it wants to stop the commit.
 
+# Allows us to read user input below, assigns stdin to keyboard
+exec < /dev/tty
+
 read -p "Did you remember to run build script? (y/n)" ANSWER
 case ${ANSWER:0:1} in
     y|Y )
-        echo Yes; exit 0
+        echo Continuing git push; exit 0
     ;;
     * )
-        echo No; exit 1
+        echo Canceled git push; exit 1
     ;;
 esac
