195afde76b4b819d2f3e827e21d9695960fb8d74
Fraasi <doofassi@gmail.com>
2020-04-23 17:40:47 +0300

    fix message

--- bin/build-reminder.sh
+++ bin/build-reminder.sh
@@ -22,7 +22,7 @@ exec < /dev/tty
 read -p "Did you remember to run build script? (y/n)" ANSWER
 case ${ANSWER:0:1} in
     y|Y )
-        echo Pushing to branch $1 at $2; exit 0
+        echo Pushing to remote $1 at $2; exit 0
     ;;
     * )
         echo Canceled git push; exit 1
