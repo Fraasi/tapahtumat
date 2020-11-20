14cb4bfc365c370a9af3e5198affeade7812f660
Fraasi <doofassi@gmail.com>
2020-11-20 22:45:01 +0200

    fix: build-reminder, bumb version

--- bin/build-reminder.sh
+++ bin/build-reminder.sh
@@ -1,4 +1,4 @@
-###!/bin/sh
+#!/bin/bash
 
 # Called by "git push" after it has checked the remote status,
 # but before anything has been pushed.  If this script exits with
@@ -19,12 +19,12 @@
 # Allows us to read user input below, assigns stdin to keyboard
 exec < /dev/tty
 
-read -p "Did you remember to run build script? (y/n)" ANSWER
+read -pr "Did you remember to run build script? (y/n)" ANSWER
 case ${ANSWER:0:1} in
     y|Y )
-        echo Pushing to remote $1 at $2; exit 0
+        echo "Pushing to remote $1 at $2"; exit 0
     ;;
     * )
-        echo Canceled git push; exit 1
+        echo "Canceled git push"; exit 1
     ;;
 esac
